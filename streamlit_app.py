e2ef718c1bc0a5e8fe14a7727596180256771664
Ayyuce Demirbas <a.ayyuced@gmail.com>
2022-02-19 21:48:55 +0300

    Create streamlit_app.py

---
+++ streamlit_app.py
@@ -0,0 +1,116 @@
+import streamlit as st
+import json
+import re
+import tensorflow_datasets as tfds
+import tensorflow as tf
+import os
+
+
+path_to_movie_lines = os.path.join('movie_lines.txt')
+path_to_movie_conversations = os.path.join('movie_conversations.txt')
+
+# Maximum number of samples to preprocess
+MAX_SAMPLES = 50000
+
+def preprocess_sentence(sentence):
+  sentence = sentence.lower().strip()
+  # creating a space between a word and the punctuation following it
+  # eg: "he is a boy." => "he is a boy ."
+  sentence = re.sub(r"([?.!,])", r" \1 ", sentence)
+  sentence = re.sub(r'[" "]+', " ", sentence)
+  # replacing everything with space except (a-z, A-Z, ".", "?", "!", ",")
+  sentence = re.sub(r"[^a-zA-Z?.!,]+", " ", sentence)
+  sentence = sentence.strip()
+  # adding a start and an end token to the sentence
+  return sentence
+
+
+def load_conversations():
+  # dictionary of line id to text
+  id2line = {}
+  with open(path_to_movie_lines, errors='ignore') as file:
+    lines = file.readlines()
+  for line in lines:
+    parts = line.replace('\n', '').split(' +++$+++ ')
+    id2line[parts[0]] = parts[4]
+
+  inputs, outputs = [], []
+  with open(path_to_movie_conversations, 'r') as file:
+    lines = file.readlines()
+  for line in lines:
+    parts = line.replace('\n', '').split(' +++$+++ ')
+    # get conversation in a list of line ID
+    conversation = [line[1:-1] for line in parts[3][1:-1].split(', ')]
+    for i in range(len(conversation) - 1):
+      inputs.append(preprocess_sentence(id2line[conversation[i]]))
+      outputs.append(preprocess_sentence(id2line[conversation[i + 1]]))
+      if len(inputs) >= MAX_SAMPLES:
+        return inputs, outputs
+  return inputs, outputs
+
+
+questions, answers = load_conversations()
+MAX_LENGTH = 40
+
+# Build tokenizer using tfds for both questions and answers
+tokenizer = tfds.features.text.SubwordTextEncoder.build_from_corpus(
+    questions + answers, target_vocab_size=2**13)
+
+# Define start and end token to indicate the start and end of a sentence
+START_TOKEN, END_TOKEN = [tokenizer.vocab_size], [tokenizer.vocab_size + 1]
+
+# Vocabulary size plus start and end token
+VOCAB_SIZE = tokenizer.vocab_size + 2
+
+model = tf.keras.models.load_model('new_chatbot_model')
+
+def evaluate(sentence):
+  sentence = preprocess_sentence(sentence)
+
+  sentence = tf.expand_dims(
+      START_TOKEN + tokenizer.encode(sentence) + END_TOKEN, axis=0)
+
+  output = tf.expand_dims(START_TOKEN, 0)
+
+  for i in range(MAX_LENGTH):
+    predictions = model(inputs=[sentence, output], training=False)
+
+    # select the last word from the seq_len dimension
+    predictions = predictions[:, -1:, :]
+    predicted_id = tf.cast(tf.argmax(predictions, axis=-1), tf.int32)
+
+    # return the result if the predicted_id is equal to the end token
+    if tf.equal(predicted_id, END_TOKEN[0]):
+      break
+
+    # concatenated the predicted_id to the output which is given to the decoder
+    # as its input.
+    output = tf.concat([output, predicted_id], axis=-1)
+
+  return tf.squeeze(output, axis=0)
+
+
+def predict(sentence):
+  prediction = evaluate(sentence)
+
+  predicted_sentence = tokenizer.decode(
+      [i for i in prediction if i < tokenizer.vocab_size])
+
+  print('Input: {}'.format(sentence))
+  print('Output: {}'.format(predicted_sentence))
+
+  return predicted_sentence
+
+
+desc = "A Simple Transformer Chatbot with Tensorflow. Check out the code [here](https://github.com/ayyucedemirbas/A_Simple_Transformer_Chatbot/blob/main/transformer_chatbot.ipynb)!"
+
+st.title('A simple chatbot')
+st.write(desc)
+
+user_input = st.text_input('Input')
+
+
+
+if st.button('Send'):
+    output = predict(str(user_input))
+    st.write(output)
